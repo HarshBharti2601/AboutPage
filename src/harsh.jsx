aecba581940812d62026e95c089e96a3134660bb
HarshBharti2601 <harshbharti2601@gmail.com>
2024-01-08 11:34:55 +0530

    initial commit

---
+++ src/harsh.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+      <div className=" m-8 text-green-800 w-full h-30 text-left sm:text-left sm:text-lg md:text-xl lg:text-2xl xl:text-3xl"> Welcome to Team Daksh
+      <p className='text-black'>content</p>
+      </div>
+      <img src="https://tse4.mm.bing.net/th?id=OIP.jmMrTb1h_-ITFtCqj9mXFwHaJQ&pid=Api&P=0&h=180"
+       className='mt-6  mr-8  mb-10  ml-8 rounded-md'/>
+      <div className='flex flex-col items-center justify-center'>
+      <div class='bg-slate-300 m-6 w-60 hover:shadow-lg  transition duration-300 ease-in-out hover:w-80 rounded-md'>
+      <h1 class="text-2xl">Topic</h1>
+      <p> content</p>
+      </div>
+      <div class="bg-slate-300 m-6 w-60 hover:shadow-lg  transition duration-300 ease-in-out hover:w-80 rounded-md">
+      <h1 class="text-2xl">Topic</h1>
+      <p> content</p>
+      </div>
+      <div class="bg-slate-300 m-6 w-60 hover:shadow-lg  transition duration-300 ease-in-out hover:w-80 rounded-md">
+      <h1 class="text-2xl">Topic</h1>
+      <p> content</p>
+      </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
